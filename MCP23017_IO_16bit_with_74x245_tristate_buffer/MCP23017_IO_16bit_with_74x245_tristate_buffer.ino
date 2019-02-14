f1c492bd27954557c6f150e764270f4578bf653d
Patrick Dudjalija <patrick.dudjalija@gmail.com>
2019-02-14 17:08:14 +0100

    arduino bus interface with 3-state control

--- MCP23017_IO_16bit_with_74x245_tristate_buffer/MCP23017_IO_16bit_with_74x245_tristate_buffer.ino
+++ MCP23017_IO_16bit_with_74x245_tristate_buffer/MCP23017_IO_16bit_with_74x245_tristate_buffer.ino
@@ -1,11 +1,10 @@
-// Control a 8-bit bus using :
+// Contrôle de bus 8-bit utilisant :
 // MCP23017 = 16bit I/O expander (I2C)
 // SN74HC245N = Octal bus transceiver with 3-state outputs
 
-// Test = light 8 leds connected on port B of SN74HC245N
-// Input = MCP23017 connected on port A of SN74HC245N
-// Arduino connected to MCP23017 (I2C protocol)
-
+// Arduino connecté à deux MCP23017 (input/output) via protocole I2C
+// INPUT = 8 interrupteurs connectés à un 3-state transceiver
+// OUTPUT = 8 leds connectée à un 3-state transceiver
 
 
 #include <Wire.h>
@@ -27,6 +26,9 @@
 
 boolean toggle_led=0;//used to toggle arduino internal led
 uint8_t valeur_switch=0; //=input sur mcp2
+uint8_t choix=0; // choix menu activation bus (O/N)
+
+
   
 void setup() {
    // put your setup code here, to run once:
@@ -57,29 +59,21 @@ void setup() {
   pinMode(ARD_LED,OUTPUT);
 
   digitalWrite(BUS_DIR1,HIGH);// DIRECTION bus1 PORTA=>PORTB
-  digitalWrite(BUS_OE1,LOW);// ~OE : output ENABLED
+  digitalWrite(BUS_OE1,HIGH);// ~OE : output DISABLED
   
   digitalWrite(BUS_DIR2,LOW);// DIRECTION bus2 PORTB=>PORTA
   digitalWrite(BUS_OE2,LOW);// ~OE : output ENABLED
+
+  //Menu pour activer/désactiver l'interface au bus des leds
+  Serial.println("Activer le bus (O/N) ? ");
+  
+
 }
+
 
 void loop() {
   
   // put your main code here, to run repeatedly:
-
-  // allumer les leds
-  for (int8_t i=0;i<8;i++){
-    Wire.beginTransmission(MCP23017_BASE_ADDR | MCP23017_ADDR1);
-    Wire.write(MCP23017_GPIOA);
-    Wire.write(1<<i);
-    Wire.endTransmission();
-    toggle_led=!toggle_led;
-    digitalWrite(ARD_LED,toggle_led);
-    delay(DELAY);
-    
-    
-    }
-
   // lire la valeur des switches
   Wire.beginTransmission(MCP23017_BASE_ADDR | MCP23017_ADDR2);
   Wire.write(MCP23017_GPIOB);
@@ -87,14 +81,44 @@ void loop() {
   Wire.requestFrom(MCP23017_BASE_ADDR | MCP23017_ADDR2,1);
   if (Wire.available()<=1)
     valeur_switch=Wire.read();
-  Serial.print("Valeur switch = ");
-  Serial.print(valeur_switch);
-  Serial.print("\n");
   
+  // allumer les leds
+  Wire.beginTransmission(MCP23017_BASE_ADDR | MCP23017_ADDR1);
+  Wire.write(MCP23017_GPIOA);
+  Wire.write(valeur_switch);
+  Wire.endTransmission();
+ 
     
+ // flasher la led interne arduino pour vérifier fonctionnement quand 
+ // le 3-state transceiver n'est pas activé ~OE1=HIGH
+ toggle_led=!toggle_led;
+    digitalWrite(ARD_LED,toggle_led);
+    delay(DELAY);
+
+
+ // traiter options reçues via moniteur série
+  if(Serial.available()>0){
+      choix=Serial.read();
+      
+      //vider le buffer pour ne pas tenir compte de CR/LF
+      while(Serial.available()>0){
+        Serial.read();
+      }
+
+
+     if (choix==79 or choix==111){
+    Serial.println("Activation du bus ...");
+    digitalWrite(BUS_OE1,LOW);// ~OE : output ENABLED
+    }
+    else if (choix==78 or choix==110){
+      Serial.println("Désactivation du bus ...");
+      digitalWrite(BUS_OE1,HIGH);// ~OE : output DISABLED
+    }
   
   
+  
+    } 
     
-  
-    
+   
+     
 }
